c4f05a8c8c962c6d1684ad0106cd3c03400fb290
EmilNadimanov <nadimaemi@gmail.com>
2020-04-19 16:57:56 +0300

    Corrected to match Norm42. Again.

--- D02.en/ex06/ft_putnbr.c
+++ D02.en/ex06/ft_putnbr.c
@@ -6,9 +6,9 @@ void ft_putchar(char c);
 
 void ft_putnbr(int nb)
 {
-	char digit; 
+	char digit;
 
-	if (nb > -10 && nb < 10) 
+	if (nb > -10 && nb < 10)
 	{
 		if (nb < 0)
 		{
@@ -17,20 +17,26 @@ void ft_putnbr(int nb)
 			ft_putchar(digit);
 		}
 		else  
-			{
-				if (nb > 0)
-					digit = nb + '0';
-				else digit = 48;
-					ft_putchar(digit);
-			}
+		{
+			if (nb > 0)
+				digit = nb + '0';
+			else digit = 48;
+				ft_putchar(digit);
+		}
 	}
 	else
 	{
 		if (nb >= 10)
 			digit = nb % 10 + '0';
 		else
-			digit = (-1 * nb) % 10 + '0';
+			digit = -1 * (nb % 10) + '0';
 		ft_putnbr(nb / 10);
 		ft_putchar(digit);
 	}
+}
+
+int main ()
+{
+	ft_putnbr(-2147483648);
+	return 0;
 }
