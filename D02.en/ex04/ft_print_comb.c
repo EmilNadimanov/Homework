c7c84c05ba6d6d9c22d2dcfdffae93d44bf30d56
EmilNadimanov <nadimaemi@gmail.com>
2020-04-19 16:22:33 +0300

    corrected to match Norm42

--- D02.en/ex04/ft_print_comb.c
+++ D02.en/ex04/ft_print_comb.c
@@ -6,7 +6,7 @@ void ft_putchar(char c);
 
 void ft_print_comb(void)
 {
-    int d1 = 48;
+	int d1 = 48;
 	int d2 = 48;
 	int d3 = 48;
 	while (d1 >= 48 && d1 <= 57)
@@ -20,24 +20,17 @@ void ft_print_comb(void)
 			{
 				if (d1 != d2 && d1 != d3 && d2 != d3)
 					{
-					ft_putchar (d1);
-					ft_putchar (d2);
-					ft_putchar (d3);
-					if (!(d1 == 57 && d2 == 56 && d3 == 55))
-						ft_putchar(44);
-					ft_putchar(32);
+						ft_putchar(d1);
+						ft_putchar(d2);
+						ft_putchar(d3);
+						if (!(d1 == 57 && d2 == 56 && d3 == 55))
+							ft_putchar(44);
+						ft_putchar(32);
 					}
 				d3++;
 			}
 			d2++;
 		}
 		d1++;
-	}
-	
-}
-
-int main()
-{
-	ft_print_comb();
-	return 0;
+	}	
 }
