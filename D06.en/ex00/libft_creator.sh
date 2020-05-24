25adc328fac110445c3ed5869a7c6c7a420a875b
EmilNadimanov <nadimaemi@gmail.com>
2020-05-24 12:52:02 +0300

    D06 Homework

--- D06.en/ex00/libft_creator.sh
+++ D06.en/ex00/libft_creator.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 gcc -c ft_putchar.c ft_swap.c ft_putstr.c ft_strlen.c ft_strcmp.c ft_strcpy.c ft_strncpy.c ft_strcat.c ft_strncat.c
 ar rc libft.a ft_putchar.o ft_swap.o ft_putstr.o ft_strlen.o ft_strcmp.o ft_strcpy.o ft_strncpy.o ft_strcat.o ft_strncat.o
-runlib libft.a
+ranlib libft.a
 rm *.o
