49c6df31d761752919a5259db789c0d977c4ee1f
EmilNadimanov <nadimaemi@gmail.com>
2020-05-24 12:52:09 +0300

    D05 Homework

--- D05.en/ex02/ft_atoi.c
+++ D05.en/ex02/ft_atoi.c
@@ -3,28 +3,28 @@
 
 int ft_atoi(char *str)
 {
-    int number;
-    int count;
-    int if_negative;
+	int number;
+	int count;
+	int if_negative;
 
-    if_negative = 0;
-    number = 0;
-    count = 0;
-    while (str[count] != '\0')
-    {
-        if ((str[count] >= '0' && str[count] <= '9')
-            || str[count] == '-' || str[count] == '+' || str [count] < 33)
-        {   
-            if (str[count] == '-' || str[count] == '+' || str [count] < 33)
-            {
-                if_negative = (str[count] == '-') ? 1 : 0;
-                count++;
-            }
-            else
-                number = number * 10 + (int)(str[count++] - 48);
-        }
-        else
-            return (if_negative) ? -number : number;
-    }
-    return (if_negative) ? -number : number;
+	if_negative = 0;
+	number = 0;
+	count = 0;
+	while (str[count] != '\0')
+	{
+		if ((str[count] >= '0' && str[count] <= '9')
+			|| str[count] == '-' || str[count] == '+' || str [count] < 33)
+		{   
+			if (str[count] == '-' || str[count] == '+' || str [count] < 33)
+			{
+				if_negative = (str[count] == '-') ? 1 : 0;
+				count++;
+			}
+			else
+				number = number * 10 + (int)(str[count++] - 48);
+		}
+		else
+			return (if_negative) ? -number : number;
+	}
+	return (if_negative) ? -number : number;
 }
--- D05.en/ex05/ft_strstr.c
+++ D05.en/ex05/ft_strstr.c
@@ -1,32 +1,30 @@
 #include <stdlib.h>
 #include <unistd.h>
-#include <stdio.h>
-#include <string.h>
 
 char *ft_strstr(char *str, char *to_find)
 {
-    int counter;
-    int found;
+	int counter;
+	int found;
 
-    counter = -1;
-    found = 0;
-    while (str[++counter] != '\0')
-    {
-        if (str[counter] == to_find[0])
-        {
-            while (to_find[found] != '\0')
-            {
-                if (str[counter + found] == to_find[found])
-                    found++;
-                else
-                {
-                    found = 0;
-                    break;
-                }
-            }
-            if (to_find[found] == '\0')
-                return str + counter;
-        }
-    }
-    return (to_find[0] == '\0') ? str : "(null)";
+	counter = -1;
+	found = 0;
+	while (str[++counter] != '\0')
+	{
+		if (str[counter] == to_find[0])
+		{
+			while (to_find[found] != '\0')
+			{
+				if (str[counter + found] == to_find[found])
+					found++;
+				else
+				{
+					found = 0;
+					break;
+				}
+			}
+			if (to_find[found] == '\0')
+				return str + counter;
+		}
+	}
+	return (to_find[0] == '\0') ? str : "(null)";
 }
